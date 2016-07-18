1c22789315bc71adb4e66c94943e6fcedf1b0e92
nlarche <nlarche@hotmail.fr>
2016-07-18 20:52:55 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+var config = {
+  entry: [
+    './index.css',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: './dist/bundle.js',
+  },
+  module: {
+    loaders: [
+    //   {
+    //     test: /\.js$/,
+    //     exclude: /node_modules/,
+    //     loaders: ['babel', 'eslint'],
+    //   },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style!css!postcss'
+        // loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+      },
+      { test: /\.svg/, loader: 'svg-url-loader' },
+        {
+        test: /\.(html|ico|jpe?g|png|gif)$/,
+        loader: "file"
+      },
+    ],
+  },
+  resolveLoader: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: "./",
+    colors: true,
+    historyApiFallback: true,
+    inline: true,
+    port: 3001,
+  }
+};
+module.exports = config;
