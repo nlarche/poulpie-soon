c924235255cbf9772ae8438215db1b8f0c7a15ab
nilar <nicolas.larche@rca.fr>
2016-07-26 10:01:50 +0200

    Add mailChimp form

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -5,6 +5,13 @@ export default class Subscribe extends React.Component {
     super();
   }
   render() {
+    const mailChimpUrl = "//poulpie.us13.list-manage.com/subscribe/post?u="
+       + "65072aaa3e4590d331363a94d&amp;id=2ad571c40b";
+
+    const fakeInputStyle = {
+      position: 'absolute',
+      left: '-5000px'
+    };
     return (
       <section className="hero is-info is-bold">
         <div className="hero-body">
@@ -14,20 +21,43 @@ export default class Subscribe extends React.Component {
                 <p className="title">Poulpie <strong>Newsletter</strong></p>
               </div>
               <div className="column">
-                <div className="control is-grouped">
-                  <div className="control has-icon">
-                    <input type="text" className="input" placeholder="email" />
-                    <i className="fa fa-envelope" ></i>
+                <form action={mailChimpUrl}
+                  method="post"
+                  name="mc-embedded-subscribe-form"
+                  className="validate"
+                  target="_blank"
+                  noValidate
+                  >
+                  <div className="control is-grouped">
+                    <div className="control has-icon">
+                      <input type="email"
+                        name="EMAIL"
+                        className="email input is-expanded"
+                        id="mce-EMAIL"
+                        placeholder="email address"
+                        />
+                      <div style={fakeInputStyle} aria-hidden="true">
+                        <input type="text"
+                          name="b_65072aaa3e4590d331363a94d_2ad571c40b"
+                          tabIndex="-1"
+                          />
+                      </div>
+                      <i className="fa fa-envelope" ></i>
+                    </div>
+                    <div className="control">
+                      <input type="submit"
+                        value="ok"
+                        name="subscribe"
+                        className="button is-primary"
+                        />
+                    </div>
                   </div>
-                  <div className="control">
-                    <input type="submit" value="ok" className="button is-primary"/>
-                  </div>
-                </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </section >
     );
   }
 }
